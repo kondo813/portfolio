312cf8ed118324de96997dc0ca66e2e535e04331
“Kondo” <prgytmt@gmail.com>
2023-09-30 13:00:52 +0900

    LPサイトデザイン修正

---
+++ blossom/script.js
@@ -0,0 +1,23 @@
+const btn4 = document.getElementById('btn4');
+const btn4Text = document.getElementById('btn4-text');
+
+btn4.addEventListener('click', () => {
+  // ボタンクリックでhiddenクラスを付け外しする
+  btn4Text.classList.toggle('hidden');
+});
+
+const btn5 = document.getElementById('btn5');
+const btn5Text = document.getElementById('btn5-text');
+
+btn5.addEventListener('click', () => {
+  // ボタンクリックでhiddenクラスを付け外しする
+  btn5Text.classList.toggle('hidden');
+});
+
+const btn6 = document.getElementById('btn6');
+const btn6Text = document.getElementById('btn6-text');
+
+btn6.addEventListener('click', () => {
+  // ボタンクリックでhiddenクラスを付け外しする
+  btn6Text.classList.toggle('hidden');
+});
