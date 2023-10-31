753f4c1f35c0f2d9b4b07409ed029c43055f627f
“Kondo” <prgytmt@gmail.com>
2023-10-31 23:10:26 +0900

    全体調整

--- usj/script.js
+++ usj/script.js
@@ -1,3 +1,23 @@
+// ヘッダー２メニュー
+// トリガー要素を取得
+const header2Triggers = document.querySelectorAll(".header2__trigger");
+
+// 各トリガーにクリックイベントを追加
+header2Triggers.forEach(header2Trigger => {
+  header2Trigger.addEventListener("click", function (event) {
+    // クリックされたトリガーの次の要素（サブメニュー）を取得
+    const submenu = event.target.nextElementSibling;
+
+    // サブメニューが表示中であれば非表示にし、非表示であれば表示する
+    submenu.classList.toggle("open");
+
+    // サブメニューの親要素からマウスが離れたとき、サブメニューを非表示にする
+    header2Trigger.parentNode.addEventListener("mouseleave", function () {
+      submenu.classList.remove("open");
+    });
+  });
+});
+
 // ハンバーガーメニューの表示・非表示を切り替えるJavaScript
 document.addEventListener('DOMContentLoaded', function () {
   const toggleButton = document.getElementById('toggleButton');
@@ -7,9 +27,6 @@ document.addEventListener('DOMContentLoaded', function () {
       menu.classList.toggle('active');
   });
 });
-
-
-
 
 $('.menu-trigger').on('click',function(){
   if($(this).hasClass('active')){
@@ -31,7 +48,6 @@ $('.overlay').on('click',function(){
   }
 });
 
-
 // ひっくり返るカード
 $(function(){
   $('.picup_content').on('click',function(){
@@ -39,25 +55,6 @@ $(function(){
   });
 });
 
-
-// トリガー要素を取得
-const header2Triggers = document.querySelectorAll(".header2__trigger");
-
-// 各トリガーにクリックイベントを追加
-header2Triggers.forEach(header2Trigger => {
-  header2Trigger.addEventListener("click", function (event) {
-    // クリックされたトリガーの次の要素（サブメニュー）を取得
-    const submenu = event.target.nextElementSibling;
-
-    // サブメニューが表示中であれば非表示にし、非表示であれば表示する
-    submenu.classList.toggle("open");
-
-    // サブメニューの親要素からマウスが離れたとき、サブメニューを非表示にする
-    header2Trigger.parentNode.addEventListener("mouseleave", function () {
-      submenu.classList.remove("open");
-    });
-  });
-});
 
 
 
